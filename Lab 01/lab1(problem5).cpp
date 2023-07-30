c446fcc579bd54c61fb41c50a048fa07631b006a
Jannatul Sifath <140313855+jsifath@users.noreply.github.com>
2023-07-30 16:30:53 +0600

    Add files via upload

---
+++ Lab 01/lab1(problem5).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    if(x>=0)
+    {
+        cout<<"x value is positive integar"<<endl;
+        if (x%2==0)
+        {
+            cout<<"x value is "<<endl;
+        if (x>999 & 10000)
+             {
+                 cout<<"x value contains digits"<<endl;
+             }
+        }
+    }
+}
