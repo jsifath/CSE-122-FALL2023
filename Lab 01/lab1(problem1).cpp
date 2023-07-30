f3d8fc39c6a622e8e734f240305f8b63d1900f9f
Jannatul Sifath <140313855+jsifath@users.noreply.github.com>
2023-07-30 15:00:33 +0600

    Add files via upload

---
+++ Lab 01/lab1(problem1).cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello in my university\n\n";
+    cout<<"I'm a student in it faculty\n\n";
+    cout<<"welcome";
+}
---
+++ Lab 01/lab1-sifath.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    cout<<"Welcome to the world of c++!!!\n";
+    cout<<"Lab1 \t";
+    cout<<"\n END\t Program\t BYE";
+    cout<<"welcome in yarmouk university\r Hellooo";
+
+}
